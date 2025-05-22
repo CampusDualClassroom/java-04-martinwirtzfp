b9ab4f2245040a1b330dabcdcf9c3f109a9c44fd
Martin Sanchez Novo <martinwirtzfp@gmail.com>
2025-05-22 11:50:54 +0200

    Ejercicio 4 realizado y testeado

--- src/main/java/com/campusdual/classroom/Exercise04.java
+++ src/main/java/com/campusdual/classroom/Exercise04.java
@@ -9,22 +9,56 @@ public class Exercise04 {
         System.out.println("Obtén el factorial del número 7 → (7!): " + (7 * 6 * 5 * 4 * 3 * 2 * 1));
         System.out.println("######################################################################");
         System.out.println("Ejemplos de cálculos CON estructuras de control:");
+        System.out.println(sumNum(5));
+        System.out.println(sumEvenNum(20));
+        System.out.println(factorial(7));
+        System.out.println(recursiveFactorial(7));
 
     }
 
     public static int sumNum(int num) {
-        return 0;
+        //Declaramos un array con tantas posiciones como números queremos sumar
+        int[] array = new int[num];
+        //Rellenamos el array
+        for (int i = 0; i<num; i++){
+            //Cada posición consiste en sumar 1 a la anterior
+            array[i] = i+1;
+        }
+        //Ya tenemos el array, ahora sumamos todas sus posiciones
+        int acumulador = 0;
+        for (int i = 0;i<num;i++){
+            acumulador = acumulador + array[i];
+        }
+        return acumulador;
     }
 
     public static int sumEvenNum(int num) {
-        return 0;
+        //Declaramos un array con las posiciones a sumar
+        int[] array = new int[num];
+
+        //Rellenamos el array
+        int numero = 0;
+        for (int i = 0; i<num; i++){
+            //Cada posición consiste en sumar 2 a la anterior
+            numero += 2;
+            array[i] = numero;
+        }
+        //Ya tenemos el array, ahora sumamos todas sus posiciones
+        int acumulador = 0;
+        for (int i = 0;i<num;i++){
+            acumulador = acumulador + array[i];
+        }
+        return acumulador;
     }
 
     public static int factorial(int num) {
-        return 0;
+        for(int i = (num-1); i>0; i--){
+            num *= i;
+        }
+        return num;
     }
 
     public static int recursiveFactorial(int num) {
-        return 0;
+        return factorial(num);
     }
 }
